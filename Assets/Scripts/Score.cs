ffddf83d90be592d6240789b55a6640500951b58
Euexia <115560425+Euexia@users.noreply.github.com>
2024-03-04 17:50:09 +0100

    StartetReload

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,36 +1,38 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
     [SerializeField] private TMP_Text textScore;
-/*    [SerializeField] private GameObject confettiPrefab;*/
-    [SerializeField] private GameObject Player1;
-    [SerializeField] private GameObject Player2;
+    [SerializeField] private GameObject endGamePanel;
+    [SerializeField] private TMP_Text textEndGame;
+    [SerializeField] private bool isGameOver = false;
+
+    public Button restartButton;
+    public Button menuButton;
+
     private int player1Score = 0;
     private int player2Score = 0;
     private int totalRounds = 0;
-    private bool isGameOver = false;
     private const int MaxRounds = 5;
 
     void Start()
     {
         UpdateScoreText();
+        endGamePanel.SetActive(false); 
+        restartButton.onClick.AddListener(RestartGame);
+        menuButton.onClick.AddListener(BackToMenu);
     }
 
     void Update()
     {
-        if (isGameOver)
+        if (totalRounds >= MaxRounds && !isGameOver)
         {
-            if (Input.GetKeyDown(KeyCode.Y))
-            {
-                RestartGame();
-            }
-            else if (Input.GetKeyDown(KeyCode.N))
-            {
-                Debug.Log("Quitting Game");
-                Application.Quit();
-            }
+            DisplayEndGame();
+            isGameOver = true;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 
@@ -41,9 +43,7 @@ public class Score : MonoBehaviour
             player1Score++;
             totalRounds++;
             UpdateScoreText();
-            /*ShowConfetti();*/
             Debug.Log("Player 1 scored! Player 1 Score: " + player1Score);
-            CheckForEndGame();
         }
     }
 
@@ -54,9 +54,7 @@ public class Score : MonoBehaviour
             player2Score++;
             totalRounds++;
             UpdateScoreText();
-            /*ShowConfetti();*/
             Debug.Log("Player 2 scored! Player 2 Score: " + player2Score);
-            CheckForEndGame();
         }
     }
 
@@ -65,39 +63,30 @@ public class Score : MonoBehaviour
         textScore.text = player1Score.ToString() + " : " + player2Score.ToString();
     }
 
-/*    private void ShowConfetti()
+    private void DisplayEndGame()
     {
-        // La logique pour afficher les confettis reste inchang�e
-        if (Player1 != null)
-        {
-            Vector3 playerPosition = Player1.transform.position;
-            Vector3 confettiPosition = playerPosition + Vector3.up * 20.0f;
-            *//*Instantiate(confettiPrefab, confettiPosition, Quaternion.identity);*//*
-            Debug.Log("Confetti spawned at position: " + confettiPosition);
-        }
-        else
-        {
-            Debug.LogWarning("Player object is null. Confetti cannot be spawned.");
-        }
-    }*/
+        endGamePanel.SetActive(true);
+        textEndGame.text = "Game Over. Final Score - Player 1: " + player1Score + ", Player 2: " + player2Score;
+    }
 
-    private void RestartGame()
+    public void RestartGame()
     {
-        // R�initialiser les variables pour recommencer le jeu
+        //Cursor.lockState = CursorLockMode.None;
+        // Réinitialiser les variables pour recommencer le jeu
+        SceneManager.LoadScene("BallTest");
         player1Score = 0;
         player2Score = 0;
         totalRounds = 0;
-        isGameOver = false;
         UpdateScoreText();
+        isGameOver = false;
+        endGamePanel.SetActive(false); // Cacher la fenêtre pop-up après le redémarrage.
+        
     }
 
-    private void CheckForEndGame()
+    public void BackToMenu()
     {
-        if (totalRounds >= MaxRounds)
-        {
-            Debug.Log("Game Over. Final Score - Player 1: " + player1Score + ", Player 2: " + player2Score);
-            Debug.Log("Press 'Y' to play again or 'N' to quit.");
-            isGameOver = true;
-        }
+        //Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("MenuScene");
+        
     }
 }
